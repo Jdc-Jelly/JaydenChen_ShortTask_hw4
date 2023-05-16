2ca1ebf93b9116ffa5c6bc427cfa71eb0667c693
Jdc-Jelly <123281090+Jdc-Jelly@users.noreply.github.com>
2023-05-15 23:08:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
 if (navigator.serviceWorker) {
-    window.addEventListener('load', () => {
-	   navigator.serviceWorker.register('./sw.js')
-	      .then(registration => console.log('SW registered'))
-		  .catch(err => console.log(`SW not registered - Error: ${err}`))
-    })
-} else {
-    console.log('Service Worker is not supported in this browser.')
+  navigator.serviceWorker.register('./sw.js')
+  
+  .then(function (registration) {
+  console.log('Registration of service worker successful with scope: ',
+  registration.scope);
+
+  }).catch(function(err) {
+  console.log('Registration of service worker failed with error: ', err);
+  
+  })} else {
+  console.log('Service Worker is not supported by this browser');
 }
