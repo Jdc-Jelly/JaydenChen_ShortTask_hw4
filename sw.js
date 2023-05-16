1dbeaf09a5727a96733f709daa976d5a83dcb75f
Jdc-Jelly <123281090+Jdc-Jelly@users.noreply.github.com>
2023-05-15 23:00:14 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_VERSION = 'myCache';
+
+const CACHE_FILES = [
+  '/Process.html',
+  '/index.html',
+  'Photo/image 1.png',
+  'Photo/image 2.png',
+  'Photo/image 3.png'
+  
+];
+
+self.addEventListener('install', event => {
+    console.log('SW installed');
+    event.waitUntil(
+        caches
+        .open(CACHE_VERSION)
+        .then(cache => {
+            console.log('SW caching files');
+            cache.addAll(CACHE_FILES)
+        })
+        .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('SW activated');
+    event.waitUntil(
+        caches.keys().then(keyNames => {
+            return Promise.all(
+                keyNames.map(key => {
+                    if(key !== CACHE_VERSION) {
+                        console.log('SW clearing old caches');
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    console.log('SW fetching');
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
